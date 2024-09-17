87201eb6f150140348567f1fe0eb6d0fd7fa3369
Claudiu H <claudiu.hasegan@gmail.com>
2024-09-17 13:25:49 +0300

    Authenticating Using Forms & Logging Users Out & Relate Posts & Comments to User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -42,23 +42,26 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
-        // $microPost1 = new MicroPost();
-        // $microPost1->setTitle('Welcome to Romania!');
-        // $microPost1->setText('...some random text here because why not ...');
-        // $microPost1->setCreated(new DateTime());
-        // $manager->persist($microPost1);
+        $microPost1 = new MicroPost();
+        $microPost1->setTitle('Welcome to Romania!');
+        $microPost1->setText('...some random text here because why not ...');
+        $microPost1->setCreated(new DateTime());
+        $microPost1->setAuthor($user1);
+        $manager->persist($microPost1);
 
-        // $microPost2 = new MicroPost();
-        // $microPost2->setTitle('Welcome to USA!');
-        // $microPost2->setText('#make America great again!');
-        // $microPost2->setCreated(new DateTime());
-        // $manager->persist($microPost2);
+        $microPost2 = new MicroPost();
+        $microPost2->setTitle('Welcome to USA!');
+        $microPost2->setText('#make America great again!');
+        $microPost2->setCreated(new DateTime());
+        $microPost2->setAuthor($user2);
+        $manager->persist($microPost2);
 
-        // $microPost3 = new MicroPost();
-        // $microPost3->setTitle('Welcome to Russia!');
-        // $microPost3->setText("We don't want war, you do :)");
-        // $microPost3->setCreated(new DateTime());
-        // $manager->persist($microPost3);
+        $microPost3 = new MicroPost();
+        $microPost3->setTitle('Welcome to Russia!');
+        $microPost3->setText("We don't want war, you do :)");
+        $microPost3->setCreated(new DateTime());
+        $microPost3->setAuthor($user1);
+        $manager->persist($microPost3);
 
         $manager->flush();
     }
