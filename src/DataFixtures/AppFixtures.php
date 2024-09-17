f0aefbd4ebe0b827cb4e7121fe4d2cbb7bc59a75
Claudiu H <claudiu.hasegan@gmail.com>
2024-09-17 12:25:50 +0300

    Hashing Passwords and Creating Users in Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,34 +3,62 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {}
     public function load(ObjectManager $manager): void
     {
+
+        $user1 = new User();
+        $user1->setEmail('test@test.com');
+        $user1->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user1,
+                '12345678'
+            )
+        );
+        $manager->persist($user1);
+
+
+        $user2 = new User();
+        $user2->setEmail('john@test.com');
+        $user2->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user2,
+                '12345678'
+            )
+        );
+        $manager->persist($user2);
+
+
         // $product = new Product();
         // $manager->persist($product);
 
-        $microPost1 = new MicroPost();
-        $microPost1->setTitle('Welcome to Romania!');
-        $microPost1->setText('...some random text here because why not ...');
-        $microPost1->setCreated(new DateTime());
-        $manager->persist($microPost1);
+        // $microPost1 = new MicroPost();
+        // $microPost1->setTitle('Welcome to Romania!');
+        // $microPost1->setText('...some random text here because why not ...');
+        // $microPost1->setCreated(new DateTime());
+        // $manager->persist($microPost1);
 
-        $microPost2 = new MicroPost();
-        $microPost2->setTitle('Welcome to USA!');
-        $microPost2->setText('#make America great again!');
-        $microPost2->setCreated(new DateTime());
-        $manager->persist($microPost2);
+        // $microPost2 = new MicroPost();
+        // $microPost2->setTitle('Welcome to USA!');
+        // $microPost2->setText('#make America great again!');
+        // $microPost2->setCreated(new DateTime());
+        // $manager->persist($microPost2);
 
-        $microPost3 = new MicroPost();
-        $microPost3->setTitle('Welcome to Russia!');
-        $microPost3->setText("We don't want war, you do :)");
-        $microPost3->setCreated(new DateTime());
-        $manager->persist($microPost3);
+        // $microPost3 = new MicroPost();
+        // $microPost3->setTitle('Welcome to Russia!');
+        // $microPost3->setText("We don't want war, you do :)");
+        // $microPost3->setCreated(new DateTime());
+        // $manager->persist($microPost3);
 
         $manager->flush();
     }
