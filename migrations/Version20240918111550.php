3816380469216efec035d23fb049d419c1c79503
Claudiu H <claudiu.hasegan@gmail.com>
2024-09-18 14:26:35 +0300

    User Checkers & Banning Users Functionality

--- migrations/Version20240918111550.php
+++ migrations/Version20240918111550.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240918074053 extends AbstractMigration
+final class Version20240918111550 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240918074053 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment ADD created DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user ADD banned_until DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment DROP created');
+        $this->addSql('ALTER TABLE user DROP banned_until');
     }
 }
