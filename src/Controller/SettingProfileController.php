25150e28a678921f1d11a3ff2da123c51c3d9313
Claudiu H <claudiu.hasegan@gmail.com>
2024-09-18 14:43:38 +0300

    User Profile Form

---
+++ src/Controller/SettingProfileController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\UserProfile;
+use App\Form\UserProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class SettingProfileController extends AbstractController
+{
+    #[Route('/setting/profile', name: 'app_setting_profile')]
+    public function profile(
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        /** @var User $user */
+        $user = $this->getUser();
+        $userProfile = $user->getUserProfile() ?? new UserProfile();
+
+        $form = $this->createForm(
+            UserProfileType::class,
+            $userProfile
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userProfile = $form->getData();
+            // $user->setUserProfile($userProfile);
+            // $em->persist($user);
+            // $em->flush();
+
+            // $this->addFlash(
+            //     'success',
+            //     'Your user profile settings were saved.'
+            // );
+
+            // return $this->redirectToRoute(
+            //     'app_settings_profile'
+            // );
+        }
+        return $this->render('setting_profile/profile.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
